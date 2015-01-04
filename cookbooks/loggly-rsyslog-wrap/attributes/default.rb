93be837ea7b67cf1eb1574474a64df939f8cb8fa
Clif Smith <clif@spanning.com>
2015-01-04 16:49:04 -0600

    loggly-rsyslog wrapper cookbook

---
+++ cookbooks/loggly-rsyslog-wrap/attributes/default.rb
@@ -0,0 +1,2 @@
+default['loggly']['rsyslog']['conf'] = '/etc/rsyslog.d/99-loggly.conf'
+
---
+++ cookbooks/loggly-rsyslog-wrap/metadata.rb
@@ -0,0 +1,9 @@
+name             'loggly-rsyslog-wrap'
+maintainer       'Clif Smith'
+maintainer_email 'clif@texicans.us'
+license          'Apache 2.0'
+description      'Configures rsyslog to send logs to Loggly'
+version          '0.0.1'
+
+depends          'loggly-rsyslog'
+
